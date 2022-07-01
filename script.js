99cec1206e3b42c4532160d06d02da3df3103490
Joel Prentiss <joel.prentiss89@gmail.com>
2022-07-01 17:07:28 -0500

    Finalish commit

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-var wrongAnswer =$('#wrong-answer');
-var rightAnswer = $('#right-answer');
+var userChoice=$('.btn')
 var startTest =$('#start-test');
+var wrong = $('.wrong-answer');
+var right = $('.right-answer');
+var score = 00;
+var secondsLeft = 60;
+var timerEl = $('.timer')
+
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      secondsLeft --;
+      timerEl.textContent = secondsLeft + 'remaining!'
+  
+      if( secondsLeft=== 0) {
+        window.alert ('Test over!');
+        window.prompt ('final score' + score);
+        window.prompt('Enter initials');
+      }
+  
+    }, 1000);
+
+  }
 
 
-wrongAnswer.on('click', function(){
-    console.log('test');
-});
+userChoice.on('click', function (){
+    // if (wrong) {
+    //     // code to reduce timer and point
+    //     score --;
+        // secondsRemain --;
+    // } 
+    if(right) {
+        score ++;
+        // code to add poits to score
+    } else{
+        score --;
+    }
+    
+}) 
+console.log(score);
+
+// collect/display user score
+// alert user of final score and ask for initials
+// ol of users 
+// keep user score in local storage
+
+
